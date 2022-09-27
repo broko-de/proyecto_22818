a3dff153aa375d939bc507d79a477bf96bd30bba
Fedex <jose.liquin@bue.edu.ar>
2022-09-27 19:43:46 -0300

    CLASE11: path, re_path, redirect, objeto request

--- cac/urls.py
+++ cac/urls.py
@@ -1,10 +1,20 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
+    path('',views.index, name='inicio' ),
     path('hola_mundo',views.hola_mundo ),
-    path('saludar/',views.saludar ),
-    path('saludar/<str:nombre>',views.saludar ),
-    path('proyectos/<int:anio>/<int:mes>',views.ver_proyectos,name="ver_proyectos" ),
+    path('saludar/',views.saludar,name='saludar_solito' ),
+    path('saludar/<str:nombre>',views.saludar,name='saludar'),
+    path('quienessomos/',views.quienes_somos,name='quienes_somos'),
+    #rutas de proyectos
+    path('proyectos/2022/08/',views.ver_proyectos_2022_08,name="ver_proyectos_2022_08"),
+    re_path(r'^proyectos/(?P<anio>\d{4})/$',views.ver_proyectos,name="ver_proyectos_anio"),
+    path('proyectos/<int:anio>/<int:mes>/',views.ver_proyectos,name="ver_proyectos" ),
+    
+    #rutas de cursos
+    path('cursos/detalle/<slug:nombre_curso>/',views.cursos_detalle,name='cursos_detalle'),
+    re_path(r'^cursos/(?P<nombre_categoria>\w{1,4})/$',views.cursos,name='cursos')
+
 ]
--- cac/views.py
+++ cac/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+from django.urls import reverse
+
+
 # Create your views here.
+def index(request):
+    if(request.method=='GET'):
+        titulo = 'Titulo cuando accedo por GET'
+    else:
+        titulo = f'Titulo cuando accedo por otro metodo {request.method}'
+    parametetros_get = request.GET.get('nombre')
+    return HttpResponse(f"""
+        <h1>{titulo}</h1>
+        <p>{parametetros_get}</p>
+    """)
+
+
+def quienes_somos(request):
+    #return redirect('saludar_solito')
+    return redirect(reverse('saludar', kwargs={'nombre': 'PepaPig'}))
+
 def hola_mundo(request):
     return HttpResponse('Hola Mundo Django 🦄')
     
@@ -10,8 +29,23 @@ def saludar(request,nombre='Fede'):
         <h1>Hola mundo Django - Un gusto {nombre}</h1>
     """)
 
-def ver_proyectos(request,anio,mes):
+def ver_proyectos(request,anio,mes=1):
     return HttpResponse(f"""
         <h1>Proyectos del {mes}/{anio}</h1>
         <p>Listado de proyectos</p>
     """)
+
+def ver_proyectos_2022_08(request):
+    return HttpResponse(f"""
+        <h1>Proyectos del mes de agosto del 2022</h1>
+        <p>Listado de proyectos</p>
+    """)
+
+def cursos_detalle(request, nombre_curso):
+    """"
+    ir a buscar en mi base datos el curso que tenga ese nombre: slug
+    """
+    return HttpResponse(f'{nombre_curso}')
+
+def cursos(request, nombre_categoria):
+    return HttpResponse(f'{nombre_categoria}')
