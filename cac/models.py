7ddae1ca4caf391fde328155b53f80caf44c16bf
Fedex <jose.liquin@bue.edu.ar>
2022-11-01 19:32:15 -0300

    Clase 21: Se agrega modelo al proyecto

--- cac/models.py
+++ cac/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Estudiante(models.Model):    
+    nombre = models.CharField(max_length=100, verbose_name='Nombre')
+    apellido = models.CharField(max_length=100, verbose_name='Apellido')
+    email = models.EmailField(max_length=100, verbose_name='Email',null=True)
+    dni= models.IntegerField(verbose_name='DNI')
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name='Nombre')
+    baja = models.BooleanField(default=0)
+
+    def __str__(self):
+        return self.nombre
+    
